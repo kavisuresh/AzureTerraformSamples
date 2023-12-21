d70e4922dfa74e798dadd1c8f92e18870267124b
Kavitha Suresh Kumar <kavi2002suresh@gmail.com>
2023-12-21 11:20:49 +0530

    Create main.tf

---
+++ WebApp/main.tf
@@ -0,0 +1,21 @@
+resource "azurerm_resource_group" "web_grp" {
+  name = "WebAppGrp"
+  location = "East US"
+}
+
+resource "azurerm_service_plan" "web_srv_plan" {
+  name = "WebAppPlan"
+  location = "East US"
+  resource_group_name = azurerm_resource_group.web_grp.name
+  os_type = "Linux"
+  sku_name = "F1"
+}
+
+resource "azurerm_linux_web_app" "web_app" {
+  name = "WebApp9875353"
+  location = "East US"
+  resource_group_name = azurerm_resource_group.web_grp.name
+  service_plan_id = azurerm_service_plan.web_srv_plan.id
+  site_config = {
+  }
+}
